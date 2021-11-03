245e34b539dd67cf036eaeac3326eb55fa999779
Joseph Diza <josephm.diza@gmail.com>
2021-11-02 21:48:59 -0700

    Remove intermediate var path_dir

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def mkdir(path, makedir=True):
     if makedir:
         os.mkdir(path)
-    return(path, str(path.stem))
+    return str(path.stem)
 
 def bang(t):
     project_name    = input('New Project Name: ')
@@ -18,24 +18,21 @@ def bang(t):
 
     # Always treat the project name as a directory
     path = Path(project_name)
+    path = path.expanduser() # Expands ~
 
-    path = path.expanduser()
-    path_dir = ''
     if (path.is_dir()):
         if (not path.exists()):
-            path_dir, project_name = mkdir(path)
+            project_name = mkdir(path)
         else:
-            path_dir, project_name = mkdir(path, makedir=False)
+            project_name = mkdir(path, makedir=False)
     elif (not path.exists()):
-        path_dir, project_name = mkdir(path)
+        project_name = mkdir(path)
     elif (path.exists()):
-        path_dir, project_name = mkdir(path, makedir=False)
+        project_name = mkdir(path, makedir=False)
     else:
         print('Project Name must be a valid name, or directory path')
         return -1
-    print(path_dir)
-    print(project_name)
-    setup = str(path_dir / 'setup.py')
+    setup = str(path / 'setup.py')
     (plumbum.cmd.echo[t.render(
         project_name=project_name,
         alias=alias,

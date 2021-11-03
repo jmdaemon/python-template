fd08995e8309e7f88debdeecfd50248cd21b7e4c
Joseph Diza <josephm.diza@gmail.com>
2021-11-02 21:40:47 -0700

    Remove dead code in template.py

--- template.py
+++ template.py
@@ -14,40 +14,31 @@ def bang(t):
     # Always treat the project name as a directory
     path = Path(project_name)
 
-    # dirname = os.path.dirname(project_name)
-
-    # path_dir = path.expanduser()
     path = path.expanduser()
     path_dir = ''
     print(path)
     if (path.is_dir()):
         if (not path.exists()):
-            # os.mkdir(path.parents[0])
             os.mkdir(path)
             path_dir = path
-            path = str(path.relative_to(path.parent.parent))
+            project_name = str(path.stem)
+            # project_name = path.name
         else:
             path_dir = path
     elif (not path.exists()):
-        # os.mkdir(path.parents[0])
         os.mkdir(path)
         path_dir = path
-        path = str(path.relative_to(path.parent.parent))
+        project_name = str(path.stem)
+        # project_name = path.name
     elif (path.exists()):
         path_dir = path
-        path = str(path.relative_to(path.parent.parent))
-    # elif (os.path.isfile(path)):
-    # elif (not path.exists()):
-        # os.mkdir(path)
-        # path = str(path.relative_to(path.parent.parent))
+        project_name = str(path.stem)
+        # project_name = path.name
     else:
         print('Project Name must be a valid name, or directory path')
         return -1
-    # setup = path / 'setup.py'
-    # setup = path.joinpath('setup.py').name
-    # setup = path_dir.joinpath('setup.py').as_posix()
     print(path_dir)
-    # setup = str(path_dir)
+    print(project_name)
     setup = str(path_dir / 'setup.py')
     (plumbum.cmd.echo[t.render(
         project_name=project_name,

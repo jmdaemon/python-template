999c743e5b4fc82dd87534eab39146d03fbc9500
Joseph Diza <josephm.diza@gmail.com>
2022-01-24 02:48:16 -0800

    Fixed unitialized outputs, create default dicts with to_dict

--- template.py
+++ template.py
@@ -1,26 +1,19 @@
 from wora.file import mkdir
-from clopy.tmpl import render, output, promptf, loadcfg, to_path, match, mkdest
+from clopy.tmpl import render, output, promptf, loadcfg, to_path, match, mkdest, init_all
 from shutil import copyfile
 import datetime
 import os
 
+def to_dict(**kwargs):
+    return kwargs
+
 def bang(fp, cmd):
-    host            = input('Where will you host [GitHub or GitLab]? [GitHub] : ') or "GitHub"
+    host            = promptf('Where will you host [GitHub or GitLab]? [{}] : ', 'GitHub')
 
-    hostcfg = {
-        'config': {
-            'alias': '',
-            'username': ''
-        }
-    }
-    gitcfg = {
-        'config': {
-            'name': '',
-            'email': ''
-        }
-    }
+    hostcfg = to_dict(config=to_dict(alias='', username=''))
+    gitcfg  = to_dict(config=to_dict(name='', email=''))
 
-    hostcfg         = loadcfg('github.toml', hostcfg) if host == "GitHub" else loadcfg('gitlab.toml', hostcfg)
+    hostcfg         = loadcfg('github.toml', hostcfg) if host == 'GitHub' else loadcfg('gitlab.toml', hostcfg)
     gitcfg          = loadcfg('git.toml', gitcfg)
 
     project_name    = promptf('What is your new project name? [{}] : ', 'app')
@@ -33,34 +26,15 @@ def bang(fp, cmd):
     cli             = promptf('Use a CLI library [Click, argparse]? [{}] : ', 'argparse')
 
     path = to_path(project_name).resolve()
-    mkdest(path)
+    mkdest(path, cmd)
     project_name = path.name
 
     # Ensure the templates render before outputting to dest
-    setupdict = {
-        'project_name': project_name,
-        'license': license,
-        'alias': alias,
-        'author': author,
-        'email': email,
-        'desc': desc,
-        'username': username,
-        'cli': cli
-    }
-    licensedict = {
-        'license': license,
-        'year': datetime.date.today().year,
-        'author': author
-    }
-    readmedict = {
-        'project_name_caps': project_name.capitalize(),
-        'desc': desc,
-        'project_name': project_name,
-        'pkgmgr': 'pip',
-        'build_sys': '',
-        'config_sys': ''
-    }
-
+    setupdict   = to_dict(project_name=project_name, license=license, alias=alias, author=author, email=email, desc=desc,
+                        username=username, cli=cli)
+    licensedict = to_dict(license=license, year=datetime.date.today().year, author=author)
+    readmedict  = to_dict(project_name_caps=project_name.capitalize(), desc=desc, project_name=project_name,
+                         pkgmgr='pip', build_sys='', config_sys='')
     tmpls = {
         'setup.py': setupdict,
         'LICENSE': licensedict,
@@ -68,7 +42,7 @@ def bang(fp, cmd):
     }
 
     # Initialize all templates
-    init_all(tmpls)
+    outputs = init_all(fp, tmpls)
 
     # Make dest directories
     mkdir(f'{path}/src/{project_name}')
